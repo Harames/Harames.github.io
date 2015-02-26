ca919c0884000d9e0b8283705cadaac7e060cd93
PKMNinja <matthew.harames@gmail.com>
2015-02-26 16:03:09 -0700

    Updates

--- js/jquery.js
+++
@@ -1,9 +0,0 @@
-// In case we ever need jquery in our project
-$( document ).ready(function() { 
-				$('div').click(function() {
-					$('.Header').fadeOut('slow');
-				});
-				$('div').click(function() {
-					$('.Header').fadeIn('slow');
-				});
-			});
